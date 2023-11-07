a2210fd15e05a6f43e3ce772144c732879bdc622
shubhjadhav1202 <148206799+shubhjadhav1202@users.noreply.github.com>
2023-11-07 15:38:31 +0530

    Create swapping.cpp

---
+++ swapping.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+ int a,b,temp;
+ cout<<"before swapping:"<<a<<endl;
+ cout<<"a="<<a<<",b="<<b<<endl;
+ temp=a;
+ a=b;
+ b=temp;
+ cout<<"after swapping:"<<endl;
+ cout<<"a="<<a<<",b="<<b<<endl;
+ return 0;
+ }
+ 
